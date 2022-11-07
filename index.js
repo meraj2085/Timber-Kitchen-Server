8e4840a08a8028efc992d683d50dc480d17393f2
meraj2085 <samirmeraj60@gmail.com>
2022-11-07 23:00:25 +0600

    testing atlas

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //All middleware
 app.use(cors());
 app.use(express.json());
 
-// async function run(){
-     
-// }
-// run()
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+     try{
+          const ServicesCollection = client.db("TimberKitchen").collection("test");
+          const user = {name: 'Meraj', age: 20, email: 'meraj123@gmail.com'}
+          const result = await ServicesCollection.insertOne(user)
+          console.log(result);
+     }
+     finally{}
+}
+run().catch((err) => console.log(err));
 
 app.get('/', (req, res)=>{
      res.send('Timber kitchen server is running...')
